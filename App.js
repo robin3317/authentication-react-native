f5b0c17cbc19827e71583e6188110460a0c98acb
robin3317 <abdurrahmanrobin47@gmail.com>
2019-03-13 14:55:43 +0600

    starting authentication with firebase

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to React Native!</Text>
+        <Text>To get started, edit App.js</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
